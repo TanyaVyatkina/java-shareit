f24ef24160adec05cad37e3381109291dcc09436
Tanya Vyatkina <tanya.vyatkina.nn@yandex.ru>
2023-12-17 14:56:16 +0300

    fix: unused import

--- src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.booking;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingRequestDto;
